1eebf903019c08f3352767414dfcdcb08f177f6a
Chandler Casey <chandlercasey13@gmail.com>
2024-06-25 09:19:48 -0500

    Added API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import * as weatherService from './services/weatherService';
+ //or use import { show } from './services/weatherService'
+
+
 import './App.css'
 
 const App = () => {
+  const fetchData = async () => {
+    const data = await weatherService.show('New York');
+    console.log('Data:', data);
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
-}
-
+};
 
 export default App
---
+++ src/services/weatherService.js
@@ -0,0 +1,20 @@
+//DO NOT CALL API FROM CLIENT SIDE LIKE DONE HERE
+
+const API_KEY = '5f1b1c97df2c403c9bc132731242506';
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+      const queryString = `&q=${city}`;
+      const res = await fetch(BASE_URL + queryString);
+      const data = await res.json();
+      console.log('Data:', data);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+
+  export { show };
